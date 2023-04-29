efc4b6a6f2dd27db5a59c1f9474ec07d0da253fa
Lo, B <39482973+lo-b@users.noreply.github.com>
2023-04-29 02:19:14 +0200

    ci: add infra for webapp (#2)
    - Add IaC for webapp using Cloud Run and setting up domain mapping.
    - Change CI/CD to correctly run.

--- main.tf
+++ main.tf
@@ -37,3 +37,72 @@ resource "google_artifact_registry_repository" "broscience_registry" {
   description   = "Docker registry for storing docker images for webapp"
   format        = "DOCKER"
 }
+
+resource "google_cloud_run_v2_service" "webapp_cloudrun_service" {
+  name     = var.cloud_run_name
+  location = var.gcp_region
+  ingress  = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "${var.gcp_region}-docker.pkg.dev/${var.gcp_project}/${var.gar_repository_id}/${var.cloud_run_image_name}:${var.cloud_run_image_tag}"
+      ports {
+        container_port = var.cloud_run_container_port
+      }
+      resources {
+        limits = {
+          cpu    = "1",
+          memory = "128Mi"
+        }
+        cpu_idle = true
+      }
+    }
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 2
+    }
+  }
+}
+
+resource "google_cloud_run_domain_mapping" "broscience_xyz_domain" {
+  location = var.gcp_region
+  name     = var.cloud_run_domain_map[0]
+
+  metadata {
+    namespace = var.gcp_project
+  }
+
+  spec {
+    route_name = google_cloud_run_v2_service.webapp_cloudrun_service.name
+  }
+}
+
+resource "google_cloud_run_domain_mapping" "www_broscience_xyz_domain" {
+  location = var.gcp_region
+  name     = var.cloud_run_domain_map[1]
+
+  metadata {
+    namespace = var.gcp_project
+  }
+
+  spec {
+    route_name = google_cloud_run_v2_service.webapp_cloudrun_service.name
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_v2_service.webapp_cloudrun_service.location
+  project  = google_cloud_run_v2_service.webapp_cloudrun_service.project
+  service  = google_cloud_run_v2_service.webapp_cloudrun_service.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
--- variables.tf
+++ variables.tf
@@ -18,3 +18,36 @@ variable "gcs_bucket_name" {
   description = "Name of the Google Cloud Storage (GCS) bucket"
   type        = string
 }
+
+variable "gar_repository_id" {
+  description = "Repository id of the Google Artifact Registry (GAR)"
+  type        = string
+}
+
+variable "cloud_run_name" {
+  description = "Name of the Cloud Run Service"
+  type        = string
+}
+
+
+variable "cloud_run_image_name" {
+  description = "Name of image run by the Cloud Run Service"
+  type        = string
+}
+
+
+variable "cloud_run_image_tag" {
+  description = "Tag of the pulled image"
+  type        = string
+}
+
+variable "cloud_run_container_port" {
+  description = "Container port of image run by Cloud Run"
+  type        = number
+}
+
+variable "cloud_run_domain_map" {
+  description = "Domains for which to create a Cloud Run domain map"
+  type        = list(string)
+  default     = ["broscience.xyz", "www.broscience.xyz"]
+}
